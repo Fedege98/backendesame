abad2655c5b13f5f295bf1883636db3c289c2b35
Federico Gemmo <94387348+Fedege98@users.noreply.github.com>
2023-07-17 12:42:33 +0200

    Add files via upload

---
+++ App.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+from azure.iot.hub import IoTHubRegistryManager
+from azure.iot.hub.models import CloudToDeviceMethod, CloudToDeviceMethodResult
+from flask_cors import CORS
+import json
+app = Flask(__name__)
+
+CORS(app)
+
+@app.route('/sendToIoTHub', methods=['POST'])
+def send_to_iothub():
+    codice = request.json.get('time')
+
+    CONNECTION_STRING = "HostName=ERAAIotHub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=GWkzgEGpfMNjP2rIIEmUqsooLIc3oDbKfq1+3cxN5pU="
+    DEVICE_ID = "ERAAGateway1"
+
+    registry_manager = IoTHubRegistryManager(CONNECTION_STRING)
+    message_json = {"time": codice}
+    registry_manager.send_c2d_message(DEVICE_ID, json.dumps(message_json))
+
+    print(f"inviato {codice}")
+
+    return jsonify({'time': codice})
+
+
+if __name__ == '__main__':
+    app.run(port=3001, debug=True)
+
+
+
+
